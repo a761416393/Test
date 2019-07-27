f4b8ae32aae6a56c68937f288f235a523a2d9776
haochen <761416393@qq.com>
2019-07-27 08:56:40 +0800

    排序与二分查找

---
+++ src/main/java/HashMao.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMao {
+    public static void main(String []args){
+        Map a = new HashMap<String,String>();
+        a.put("sda","sdsa");
+    }
+
+}
